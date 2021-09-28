ad9fe1e741c8eaaf82a3ca6e6b73517fdabdd9d8
Jason Schlachet <jss@offramp.org>
2021-09-28 07:52:43 -0700

    moved more code into lib. code is too far to BEQ/BNE now, so switched to JMP. also replaced JSR with JMP, solving stack problem.

--- src/dualvia-serial.s
+++ src/dualvia-serial.s
@@ -1,13 +1,13 @@
 ;
 
-.setcpu "65C02"
+  .setcpu "65C02"
 
-ZP_MESSAGE   = $08 ; message to send via serial
 
   .include "acia.cfg"
   .include "lcd-4bit.cfg"
   .include "via.cfg"
 
+ZP_MESSAGE   = $08 ; message to send via serial
 
 message_startup: .byte $0d, $0a, "Starting up.", $0d, $0a, $00 ; CR LF NULL
 message_empty: .byte "Buffer empty.", $0d, $0a, $00
@@ -45,13 +45,6 @@ reset:
 loop:
   jmp loop
 
-
-
-
-
-
-
-
 nmi:
   rti
 
@@ -65,16 +58,29 @@ irq:
   LDA ACIA_DATA
 
   CMP #$1b              ; escape
-  BEQ key_escape
+  BNE key_escape_continue
+  JMP key_escape
+key_escape_continue:
+
   CMP #$7f              ; backspace
-  BEQ key_backspace
+  BNE key_backspace_continue
+  JMP key_backspace
+key_backspace_continue:
+  
   ; CMP #$0d             ; enter
   ; BEQ key_enter
-  CMP #$60              ; backtick
-  BEQ key_backtick
-  CMP #$03              ; Control-C
-  BEQ perform_reset
 
+  CMP #$60              ; backtick
+  BNE key_backtick_continue
+  JMP key_backtick
+key_backtick_continue:
+ 
+  CMP #$03              ; Control-C
+  BNE perform_reset_continue
+  JMP perform_reset
+perform_reset_continue:
+
+  ; all other keys, ...
   JSR write_acia_buffer
   JSR print_char
 
@@ -102,92 +108,6 @@ irq_end:
   PLX
   PLA
   RTI
-
-perform_reset:
-  JMP reset
-
-key_escape:             ; $f0
-  LDA #%00000001        ; Clear display
-  JSR lcd_instruction
-  LDA ACIA_WR_PTR       ; load write pointer
-  STA ACIA_RD_PTR       ; store to read pointer (empty buffer)
-  LDA #$0d              ; ASCII CR
-  STA ACIA_DATA
-  JSR delay_6551
-  LDA #$0a              ; ASCII LF
-  STA ACIA_DATA
-  JSR delay_6551
-  JMP irq_reset_end
-key_backspace:          ; $7f
-  LDA #%00010000        ; move cursor left
-  JSR lcd_instruction
-  LDA #$20              ; print space
-  JSR print_char
-  LDA #%00010000        ; move cursor left
-  JSR lcd_instruction
-  LDA ACIA_WR_PTR       ; load write pointer
-  DEC                   ; decrement by one
-  STA ACIA_WR_PTR       ; save pointer back out
-  JSR send_backspace_serial
-  jmp irq_reset_end
-key_backtick:
-  LDA #%00000001        ; Clear display
-  JSR lcd_instruction
-  JSR print_buffer
-  JMP irq_reset_end
-
-send_backspace_serial:
-  LDA #$08              ; ASCII BS
-  STA ACIA_DATA
-  JSR delay_6551
-  LDA #$20              ; ASCII space
-  STA ACIA_DATA
-  JSR delay_6551
-  LDA #$08              ; ASCII BS
-  STA ACIA_DATA
-  JSR delay_6551
-  RTS
-
-print_buffer_contents:
-  PHA
-  PLA
-  RTS
-
-print_buffer:
-  PHA
-  PHX
-
-  JSR acia_buffer_diff
-  BEQ print_buffer_empty
-
-  JSR set_message_buffer
-  JSR send_message
-  ;
-read_acia_buffer_loop:
-  JSR read_acia_buffer        ; read char from buffer
-  STA ACIA_DATA              ; output to serial console
-  JSR delay_6551
-  ; move read pointers
-  INC ACIA_RD_PTR
-  ;
-  JSR acia_buffer_diff        ; check pointer different
-  BEQ read_acia_buffer_end    ; if buffer empty then exit
-  JSR read_acia_buffer_loop
-read_acia_buffer_end:
-  ;
-  JSR set_message_crlf
-  JSR send_message
-  JMP print_buffer_end
-  ;
-print_buffer_empty:
-  JSR set_message_empty
-  JSR send_message
-  JMP print_buffer_end
-  ;
-print_buffer_end:
-  PLX
-  PLA
-  RTS
 
 
 
